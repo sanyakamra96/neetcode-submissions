84c48a5b20ce799a3f488aaa08ac56c99454fbb6
sanyakamra96 <coolbuddysanya@gmail.com>
2026-06-09 13:32:00 -0700

    Add: meeting-schedule-ii - submission-6

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-6.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        if (intervals.empty()) return 0;
+        sort(intervals.begin(),intervals.end(),[](Interval& a, Interval& b){
+            return a.start<b.start;
+        });
+       priority_queue<int,vector<int>,greater<int>> min_pq;
+        int n = intervals.size();
+        int conflict=1;
+       for(int i = 0;i<n;i++){
+        if (!min_pq.empty()){
+            if(intervals[i].start<min_pq.top()){
+                        conflict++;
+                    }
+            else{
+                min_pq.pop();
+
+            }
+        }
+        
+        min_pq.push(intervals[i].end);
+       }
+       return conflict;
+    }
+};
